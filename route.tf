2251b2f1915588a851168cec997428d4d2ca0465
paolocalvino@gmail.com <paolo.calvino@cloudreach.com>
2022-09-16 13:48:07 +0200

    Adding route public and private route tables

--- route.tf
+++ route.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table" "tf_route" {
+resource "aws_route_table" "tf_route_public" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -6,8 +6,42 @@ resource "aws_route_table" "tf_route" {
     gateway_id = aws_internet_gateway.terraform-igw.id
   }
 }
+resource "aws_route_table" "tf_route_private" {
+  vpc_id = aws_vpc.main.id
 
-resource "aws_route_table_association" "tf_routeassoc" {
-  subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.tf_route.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.tf_gateway.id
+  }
 }
+
+resource "aws_route_table_association" "tf_pub_routeassoc_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.tf_route_public.id
+}
+
+resource "aws_route_table_association" "tf_pub_routeassoc_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.tf_route_public.id
+}
+
+resource "aws_route_table_association" "tf_pub_routeassoc_3" {
+  subnet_id      = aws_subnet.public_3.id
+  route_table_id = aws_route_table.tf_route_public.id
+}
+
+resource "aws_route_table_association" "tf_pri_routeassoc_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.tf_route_private.id
+}
+
+resource "aws_route_table_association" "tf_pri_routeassoc_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.tf_route_private.id
+}
+
+resource "aws_route_table_association" "tf_pri_routeassoc_3" {
+  subnet_id      = aws_subnet.private_3.id
+  route_table_id = aws_route_table.tf_route_private.id
+}
+
