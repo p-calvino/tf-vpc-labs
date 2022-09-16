5c13fc694bfd995b26033c0b7f97e9982aa74e19
paolocalvino@gmail.com <paolo.calvino@cloudreach.com>
2022-09-16 09:31:03 +0200

    Working version

--- subnets.tf
+++ subnets.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "public_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.1.0/24"
   availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Public-Subnet-1"
@@ -12,6 +13,7 @@ resource "aws_subnet" "public_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.2.0/24"
   availability_zone = "eu-central-1b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Public-Subnet-2"
@@ -23,6 +25,7 @@ resource "aws_subnet" "public_3" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.3.0/24"
   availability_zone = "eu-central-1c"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Public-Subnet-3"
